56116625bf24e94d39d7165f8fca11bdc1d84539
347772108@qq.com <gitee.933910>
2020-07-30 23:11:55 +0800

    update

--- src/main/java/org/yzh/framework/commons/transform/Bin.java
+++ src/main/java/org/yzh/framework/commons/transform/Bin.java
@@ -8,22 +8,43 @@ package org.yzh.framework.commons.transform;
  */
 public class Bin {
 
-    private static final int[] bits = new int[33];
+    private static final int[] bits = new int[32];
 
     static {
-        bits[1] = 1;
-        for (int i = 2; i < bits.length; i++) {
-            bits[i] = bits[i - 1] * 2;
+        bits[0] = 1;
+        for (int i = 1; i < bits.length; i++) {
+            bits[i] = bits[i - 1] << 1;
         }
     }
 
-    /** 读取n的第i位 */
+    /**
+     * 读取n的第i位
+     *
+     * @param n int32
+     * @param i 取值范围0-31
+     */
     public static boolean get(int n, int i) {
         return (n & bits[i]) == bits[i];
     }
 
-    /** 写入bool到n的第i位 */
+    /**
+     * 写入bool到n的第i位
+     *
+     * @param n int32
+     * @param i 取值范围0-31
+     */
     public static int set(int n, int i, boolean bool) {
         return bool ? n | bits[i] : n ^ (n & bits[i]);
+    }
+
+    /**
+     * 按位写入int
+     * 数组第一个int为首位
+     */
+    public static int writeInt(int... bit) {
+        int r = 0;
+        for (int i = 0; i < bit.length; i++)
+            r = bit[i] > 0 ? (r | bits[i]) : (r ^ (r & bits[i]));
+        return r;
     }
 }
